0218ca51ceeeee93eae4c4c5a60aff1591b13594
Collin Shook <collin.shook@verizon.com>
2019-07-23 12:00:27 -0400

    your commit message

---
+++ strings/stringmethod02.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Alta3 Research || Atuhor: RZFeeser@alta3.com"""
+def main():
+    """ Run-Time ciode """
+    lilstring = "Alta3 Research offers classes on Python coding"
+    newlist = lilstring.split(" ")
+    print(newlist)
+
+    myiplist = ["192", "168", "0", "12"]
+
+    singleip = ".".join(myiplist)
+
+    print(singleip)
+
+main()
+
---
+++ strings/stringmethod11.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+lilstring = "alta3 research offers classes on python coding"
+newlist = lilstring.split(" ")
+print(newlist)
+
+myiplist = ["192", "168", "0", "12"]
+
+singleip = ".".join(myiplist)
+
+print(singleip)
+
