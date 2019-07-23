dd15bc76dc6c4d5f34e4934bb3a58d1af41fe976
Collin Shook <collin.shook@verizon.com>
2019-07-23 07:20:41 -0400

    reason for commit

---
+++ iptaker02.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+user_input = input("Please enter an IP address:")
+## the line below creates a single string that is passed to print()
+# print("you told me the IPV4 address is :" + user_input)
+## print() can be given a series of objects seperated by a coma 
+##print() can be given a series of objects seperated by a coma
+print("You told me the IPV4 address is: " , user_input)
+vendor_name = input("enter vendor name:")
+print("your vendor is:" , vendor_name)
+
---
+++ mix-list/mixlist01.py
@@ -0,0 +1,9 @@
+
+#!/usr/bin/env python3
+my_list = ["192.168.0.5",5060, "up"]
+print("The first itme in the list (IP):" + my_list[0])
+print("The second item in the list(Port):" + str(my_list[1]))
+print("The last item in the lsit(state):" + my_list[2])
+new_list = [5060,"80",55,"10.20.30.1","10.20.20.1","ssh"]
+When I ssh into IP addresses 10.0.0.1 or 10.20.30.1 I am unable to ping ports 5-6-
+
